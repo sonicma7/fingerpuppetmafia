22c8ec62c97509558b295673f7bc2280340c83d0
tbennet2 <tbennet2@c3bef888-a3c8-11de-b195-ab392a2a73f5>
2009-11-12 03:51:27 +0000

    Updates: I updated the maps hopefully for the last time, along with the directions.  Also made the shuttles visible, and kept the zoom in and zoom out feature working.  Also added comments for all of my section in accordance with our policies.

--- Jmo/src/SVGSimpleMidlet.java
+++ Jmo/src/SVGSimpleMidlet.java
@@ -15,9 +15,12 @@ import javax.microedition.lcdui.TextBox;
 import javax.microedition.lcdui.TextField;
 import javax.microedition.midlet.*;
 
-
-
+/* SVGSimpleMidlet sets up the basic menus, listeners for those menus,
+ * different threads for the program and generally controls the application
+ * Able to destroy, pause and start application here
+ */
 public class SVGSimpleMidlet extends MIDlet implements CommandListener {
+    //set up variables for new svgCanvas, menus, parser
     SVGStaticCanvas svgCanvas = null;
     Command exit,zoomin,zoomout,cmd1,getstop,getshuttle;
     LocPosition lp;
@@ -28,22 +31,31 @@ public class SVGSimpleMidlet extends MIDlet implements CommandListener {
     Display disp;
     Thread canvThr,lpThr = null, sdThr = null, parThr = null;
     List list= null;
+
+    /*startApp is responsible for starting the application, adding in commands
+     * and starting different threads and command listeners
+     */
     public void startApp() {
+        //set up display
         disp  = Display.getDisplay(this);
         //svgCanvas = getCanvas();
+        //set up menus
         cmd1 = new Command("Back",Command.BACK, 0);
         tb1 = new TextBox(" "," ",150, TextField.ANY );
         tb2 = new TextBox(" "," ", 150, TextField.ANY );
 
+        //set up locations, simulate data and parser
         lp = new LocPosition(tb1);
         sd = new SimulateData(tb2);
         par = new Parser();
 
+        //set command listeners for text boxes
         tb1.addCommand(cmd1);
         tb1.setCommandListener(this);
         tb2.addCommand(cmd1);
         tb2.setCommandListener(this);
 
+        //sets up threads for location position and simulate data
         lpThr = new Thread(lp);
         sdThr = new Thread(sd);
 
@@ -58,12 +70,16 @@ public class SVGSimpleMidlet extends MIDlet implements CommandListener {
         disp.setCurrent(getList());
     }
 
+
     public void pauseApp() {
     }
 
     public void destroyApp(boolean unconditional) {
     }
 
+    /* commandAction takes in a command a displayable and queues it in a list
+     * It also sets up a way to display this list
+     */
     public void commandAction(Command command, Displayable displayable) {
         if (command == cmd1) {
             disp.setCurrent(getList());
@@ -80,8 +96,10 @@ public class SVGSimpleMidlet extends MIDlet implements CommandListener {
      * stop, find closest shuttle
      */
     public SVGStaticCanvas getCanvas(int canv){
+        //set up canvas as long as it is null
         if (svgCanvas == null){
             svgCanvas = new SVGStaticCanvas(true, lp, canv);
+            //set up menu with exit, zoom in/out, getstop and getshuttle
             exit = new Command("Exit", Command.SCREEN, 0);
             zoomin = new Command("Zoom in", Command.SCREEN, 1);
             zoomout = new Command("Zoom out", Command.SCREEN, 1);
@@ -93,7 +111,8 @@ public class SVGSimpleMidlet extends MIDlet implements CommandListener {
             svgCanvas.addCommand(getstop);
             svgCanvas.addCommand(getshuttle);
             svgCanvas.setCommandListener(new CommandListener() {
-                /* CommandAction listens for menu options once in the campus map, such as
+
+                /* commandAction listens for menu options once in the campus map, such as
                 * zoom in, zoom out, find closest shuttle stop and find closest shuttle.
                 */
                 public void commandAction(Command c, Displayable d) {
@@ -103,10 +122,10 @@ public class SVGSimpleMidlet extends MIDlet implements CommandListener {
                         disp.setCurrent( getList());
                     }
                     else if(c == zoomin) {
-                            svgCanvas.zoomin();
+                            svgCanvas.zoomIn();
                         }
                     else if(c== zoomout) {
-                                svgCanvas.zoomout();
+                                svgCanvas.zoomOut();
                     }
                     else if(c== getstop){
                     }
@@ -124,7 +143,7 @@ public class SVGSimpleMidlet extends MIDlet implements CommandListener {
     */
     public List getList() {
         if (list == null) {
-            // write pre-init user code here
+            // sets up list for getting location, different maps, data and exit
             list = new List("list", Choice.IMPLICIT);
             list.append("Get Location",null);
             list.append("See Campus",null);
@@ -137,7 +156,10 @@ public class SVGSimpleMidlet extends MIDlet implements CommandListener {
         }
         return list;
     }
-    
+
+    /*listAction takes in the menu options set up in getList and reacts to them
+     * if selected, displays correct map, reacts to get and simulate data
+     */
     void listAction(){
         String __selectedString = getList().getString(getList().getSelectedIndex());
         int route = 0;      //used to determine what route is selected
--- Jmo/src/SVGStaticCanvas.java
+++ Jmo/src/SVGStaticCanvas.java
@@ -1,7 +1,6 @@
 
 import java.io.InputStream;
 import java.util.Vector;
-import java.util.Enumeration;
 import javax.microedition.lcdui.Graphics;
 import javax.microedition.lcdui.game.GameCanvas;
 import javax.microedition.m2g.SVGImage;
@@ -10,33 +9,49 @@ import org.w3c.dom.svg.SVGPoint;
 import org.w3c.dom.svg.SVGRect;
 import org.w3c.dom.svg.SVGSVGElement;
 
+/*SVGStaticCanvas loads the scalable vector graphics files when called, uses Graphics g,
+ * Paint, repaint, zoom in and out, moving between the stops as well as running the 
+ * main part of the program with threads, sleeping them when necessary
+ */
 class SVGStaticCanvas extends GameCanvas implements Runnable {
     private SVGImage svgImage = null;
-    double minlong = -73.66212844848633f;
-    double maxlong = -73.69083881378174f;
-    double minlat = 42.72207861996528f;
-    double maxlat = 42.73913286689956f;
+    /*following variables are used in order to correlate the gps coordinates to
+     * the pixels on the svg images
+     */
+    double minlong = -73.66281509399414f;
+    double maxlong = -73.68903636932373f;
+    double minlat = 42.72204709206166f;
+    double maxlat = 42.73903830914378f;
     double longvar = 240/(maxlong - minlong);
-    double latvar = 400/(maxlat - minlat);
+    double latvar = 450/(maxlat - minlat);
 
+    //the following variables set up the SVG image and deal with positioning
     private ScalableGraphics gc;
     SVGSVGElement myEl = null;
     SVGPoint p1;
     SVGRect myRect;
-    float f1,fw,fh;
+    float f1,fw,fh,fz;
     float globx=0, globy=0;
     int xcord=0, ycord=0;
     LocPosition mylp;
+    float xoffset = 0, yoffset = 0;
+
+    //the following variables set up, call and slow down the parser
+    Parser myparse = new Parser();
+    Vector shuttles;
+    int counter = 0;
+    
 
     /* SVGStaticCanvas sets up our canvases, depending on which canvas we want called.
      * Canvas 1 = East bus route, 2 = West bus route, else regular campus map
-     *
+     * Takes in a key event, Location and a canvas variable
      */
     protected   SVGStaticCanvas(boolean supKeyEvent,LocPosition lp, int canvas ) {
         super(true);
         mylp = lp;
         gc = ScalableGraphics.createInstance();
         this.setFullScreenMode(true);
+        //depending on what canvas number is, try and create the image
         if(canvas == 1){
             try{
                InputStream is = getClass().getResourceAsStream("East.svg");
@@ -72,7 +87,12 @@ class SVGStaticCanvas extends GameCanvas implements Runnable {
         fw=myRect.getWidth();
         fh=myRect.getHeight();        
     }
-    public void paint(Graphics g){                
+
+    /*Paint is responsible for refreshing the SVG image, displaying the user
+     * location, as well as the locations of all of the shuttles
+     */
+    public void paint(Graphics g){
+        //set up initial SVG image with background
         g.setColor(0x00ffff50);
         g.fillRect(0, 0, getWidth(), getHeight());    
         g.setColor(0x00ffff50);
@@ -80,107 +100,167 @@ class SVGStaticCanvas extends GameCanvas implements Runnable {
         g.setColor(0x00ff5050);        
         gc.render(0, 0, svgImage);
         g.setColor(0x0ffffff);
+        //Currently a person is a square
         g.fillRect(xcord + (int)(globx), ycord + (int)(globy),8,8);
-        gc.releaseTarget();    
+        gc.releaseTarget();
+
+        //as long as we have data for the shuttles, try and get them
+        if(shuttles != null){
+            //x and y coordinate variables for the x and y location of shuttles
+            double x;
+            double y;
+            //loops through shuttles, takes zoom into account
+            for (int q = 0; q < shuttles.size(); q++){
+                 x = (((Shuttle)(shuttles.elementAt(q))).getCoordinates()[0] - minlong) * longvar + xoffset;
+                 y = (((Shuttle)(shuttles.elementAt(q))).getCoordinates()[1] - minlat) * latvar + 100 - yoffset;
+
+                //displays the shuttles as a rectangle
+                gc.bindTarget(g);
+                g.setColor(0x0000000);
+                g.fillRect((int)x,(int)y,8,8);
+                gc.releaseTarget();
+            }
+
+        }
+        
     }
 
-    public void updatebus(Vector shuttles, Graphics g) {
-        double x;
-        double y;
-        Shuttle s1;
-        gc.bindTarget(g);
-        gc.render(0, 0, svgImage);
-        gc.releaseTarget();
-        for (int q = 0; q < shuttles.size(); q++){
-            x = - (((Shuttle)(shuttles.elementAt(q))).getCoordinates()[0] - minlong) * longvar;
-            y = (((Shuttle)(shuttles.elementAt(q))).getCoordinates()[1] - minlat) * latvar + 100;
-            gc.bindTarget(g);
-            g.setColor(0x0000000);
-            g.fillRect((int)x,(int)y,8,8);
-            gc.releaseTarget();
-        }
-    }
-        
-    public void zoomin() {
+    /* zoomIn is responsible for the zoom in case, gets current svg point coordinates
+     * scales it by 1.2, subtracts current scale from that, then modifies the x and y coordinates.
+     * Also responsible for updating the x and y offset for the busses and repaints
+     */
+    public void zoomIn() {
+        //get old coordinates
         float fa =  myEl.getCurrentScale();
         p1= myEl.getCurrentTranslate();
         float fx = p1.getX();
         float fy=p1.getY();
+        //update scale
         myEl.setCurrentScale(myEl.getCurrentScale() * 1.2f);
-        float fz= myEl.getCurrentScale() - fa;   
-        float transx= fx - (fw/2)*fz;
+        fz= myEl.getCurrentScale() - fa;
+        //update new coordinates
+        float transx = fx - (fw/2)*fz;
         float transy =  fy - (fh/2)*fz ;
         p1.setX( transx);
         p1.setY(transy);
+        //update bus coordinates
+        xoffset -= (fw/4.5)*fz;
+        yoffset -= (fw/7)*fz;
         repaint();
     }
 
-    public void zoomout() {
+    /* zoomOut is responsible for the zoom in case, gets current svg point coordinates
+     * scales it by 1.2, subtracts that from current scale, then modifies the x and y coordinates.
+     * Also responsible for updating the x and y offset for the busses and repaints
+     */
+    public void zoomOut() {
+        //get old coordinates
         float fa =  myEl.getCurrentScale();
         p1= myEl.getCurrentTranslate();
         float fx = p1.getX();
         float fy=p1.getY();
+        //update scale
         myEl.setCurrentScale(myEl.getCurrentScale() /(1.20f));
         float fz= fa - myEl.getCurrentScale() ;
+        //update new coordinates
         float transx= fx + (fw/2)*fz;
         float transy =  fy + (fh/2)*fz ;
         p1.setX( transx);
         p1.setY(transy);
+        //update bus coordinates
+        xoffset += (fw/4.5)*fz;
+        yoffset += (fw/7)*fz;
         repaint();
     }
+
+    /* run is responsible for initializing key states, listening for key presses,
+     * getting longitude and latitude and keeping track of threads for downloading
+     * the new data as well as when to display the shuttles, also repaints
+     */
     public void run() {
         while( true ) {
+            //set up key states, current position and check for key presses
             int keyState = getKeyStates();
             p1= myEl.getCurrentTranslate();
             float fx= p1.getX();
             float fy=p1.getY();
 
+            //if key is pressed, react correctly (move screen at the moment)
             if ((keyState & LEFT_PRESSED) != 0) {
                 p1.setX(fx + 10);
+                xoffset += 10;
                 globx+= 10;
                 keyState = 0;
             }
             if ((keyState & RIGHT_PRESSED) != 0) {
                 p1.setX(fx - 10);
+                xoffset -= 10;
                 globx-=10;
                 keyState=0;
             }
             if ((keyState & UP_PRESSED) != 0) {
                 p1.setY(fy + 10);
-                globy+=10;
+                yoffset -= 10;
+                globy-=10;
                 keyState=0;
             }
             if ((keyState & DOWN_PRESSED) != 0) {
                 p1.setY(fy - 10);
-                globy-=10;
+                yoffset += 10;
+                globy+=10;
                 keyState=0;
             }
+            //get latitude and longitude and calculate coordinate
             double lat = mylp.getLatitude();
             double longit = mylp.getLongitude();
             calc(lat,longit);
+            //sleep the thread for 50ms
             try{
                 Thread.sleep(50);
+                //after 2500 ms, start the parser
+                if (counter == 50){
+                    counter++;
+                    myparse.go();
+                }
+                //this is so we do not get new data every 50ms
+                else if (counter < 500 ){
+                    counter++;
+                }
+                //after 25000ms, evaluate the shuttles and display them
+                else{
+                    counter = 0;
+                    System.out.println("Something happened!!!!");
+                    shuttles = myparse.getShuttles();
+                    repaint();
+                    
+                }
             }
-            catch(Exception e){};
+            catch(Exception e){
+            System.out.println("Exception: " + e);
+            }
             repaint();
         }
     }
 
+    /*calc takes in longitude and latitude and calculates their respective locations
+     * with regard to the SVG image in pixels
+     */
     void calc(double lat, double longit){   
-        double z = ( (lat - minlat)/(maxlat -minlat))*400;
+        double z = ( (lat - minlat)/(maxlat -minlat))*450;
         double r = ((longit - minlong)/(maxlong - minlong))*240;
-         
+
+        //check to see if we are in bounds
         if (z < 0)
             z = 0;
-        if (z > 400.0)
-            z = 400.0;
+        if (z > 450.0)
+            z = 450.0;
          if (r <0)
              r = 0;
          if (r > 240.0)
              r = 240.0;
-         z = myEl.getCurrentScale()*(z -200) + 200;
+         z = myEl.getCurrentScale()*(z -225) + 225;
          r  = myEl.getCurrentScale()*(r - 120) + 120;
          xcord = (int) z;
-         ycord = (int) r;
+         ycord = (int) r + 100;
      }
 }
